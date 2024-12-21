3a49ef0fda0471fc57e389801f6b9a31f37a9a03
Phoshoko Thabang <phoshokothabang1@gmail.com>
2024-12-21 13:56:12 +0200

    Replaced the old variables with the new once

--- 2-variables/piggy-bank/currency.cpp
+++ 2-variables/piggy-bank/currency.cpp
@@ -6,13 +6,13 @@ int main()
   double pesos, reais, soles, dollars;
 
   std::cout << "🇲🇽 Enter number of Pesos: ";
-  std::cin >> p;
+  std::cin >> pesos;
 
   std::cout << "🇬🇹 Enter number of Guatemalan Quetzals: ";
-  std::cin >> q;
+  std::cin >> reais;
 
   std::cout << "🇸🇻 Enter number of Salvadoran Colons: ";
-  std::cin >> c;
+  std::cin >> soles;
 
   dollars = 0.049 * p + 0.1305 * q + 0.1144 * c;
 
